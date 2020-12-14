44240241d22448bbebc3dcb8ac427141825757e4
drHaithamMG <33106055+drHaithamMG@users.noreply.github.com>
2020-12-14 23:45:37 +0200

    11:45

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 // Prevent user from entering white spaces on first/last name field
 document.querySelectorAll('.checkEneteredChar').forEach(item => {
-    item.addEventListener('keypress', function (event) {
-        let key = event.keyCode;
-        if (key === 32) {
-            event.preventDefault();
-        }
+        item.addEventListener('keypress', function(event) {
+            let key = event.keyCode;
+            if (key === 32) {
+                event.preventDefault();
+            }
+        })
     })
-})
-//Prevent user from entering numbers 
+    //Prevent user from entering numbers 
 const alphaOnly = document.querySelector('.city')
 
 alphaOnly.addEventListener('keypress', event => {
-    let key = event.keyCode;
-    if (key >= 48 && key <= 57) {
-        event.preventDefault();
-    }
-})
-//force the user to enter only-digit inputs
+        let key = event.keyCode;
+        if (key >= 48 && key <= 57) {
+            event.preventDefault();
+        }
+    })
+    //force the user to enter only-digit inputs
 document.querySelectorAll('.number').forEach(item => {
     item.addEventListener('keypress', event => {
         let key = event.keyCode;
@@ -42,14 +42,13 @@ function checkEmail(email) {
  * @return {void}
  */
 function checkOther() {
-    const other = document.querySelector('.other');
+    const other = document.querySelector('.hidden');
     const option = document.getElementById('select1');
     let value = option.value;
     if (value == '4') {
         option[5].text = "Other (Please specify...)"
-        other.style.display = 'inline-block';
-    }
-    else {
+        other.style.display = 'block';
+    } else {
         option[5].text = "Other"
         other.style.display = 'none';
     }
