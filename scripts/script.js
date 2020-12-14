17bfd89b771685ebafedb5c5e076510e73161ed5
drhaithammg <dr.haithammg@gmail.com>
2020-12-14 13:32:08 +0200

    3

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+// Prevent user from entering white spaces on first/last name field
+document.querySelectorAll('.checkEneteredChar').forEach(item => {
+    item.addEventListener('keypress', function (event) {
+        let key = event.keyCode;
+        if (key === 32) {
+            event.preventDefault();
+        }
+    })
+})
+//Prevent user from entering numbers 
+const alphaOnly = document.querySelector('.city')
+
+alphaOnly.addEventListener('keypress', event => {
+    let key = event.keyCode;
+    if (key >= 48 && key <= 57) {
+        event.preventDefault();
+    }
+})
+//force the user to enter only-digit inputs
+document.querySelectorAll('.number').forEach(item => {
+    item.addEventListener('keypress', event => {
+        let key = event.keyCode;
+        if (key < 48 || key > 57) {
+            event.preventDefault();
+        }
+    })
+})
+
+/**
+ * Email check vailidity
+ * @param {string} email 
+ * @returns {boolean} 
+ */
+function checkEmail(email) {
+    //https://www.w3resource.com/javascript/form/email-validation.php
+    const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+}
+/**
+ * show 'other' section if it selected from the drop down menu
+ * @param {void}
+ * @return {void}
+ */
+function checkOther() {
+    const other = document.querySelector('.other');
+    const option = document.getElementById('select1');
+    let value = option.value;
+    if (value == '4') {
+        option[5].text = "Other (Please specify...)"
+        other.style.display = 'inline-block';
+    }
+    else {
+        option[5].text = "Other"
+        other.style.display = 'none';
+    }
+
+
+}
