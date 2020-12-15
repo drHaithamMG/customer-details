2a3e9afe9502449760fd0634e9781dcd8d83e746
drhaithammg <dr.haithammg@gmail.com>
2020-12-15 19:01:27 +0200

    last-day

--- scripts/script.js
+++ scripts/script.js
@@ -26,16 +26,7 @@ document.querySelectorAll('.number').forEach(item => {
     })
 })
 
-/**
- * Email check vailidity
- * @param {string} email 
- * @returns {boolean} 
- */
-function checkEmail(email) {
-    //https://www.w3resource.com/javascript/form/email-validation.php
-    const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-}
 /**
  * show 'other' section if it selected from the drop down menu
  * @param {void}
@@ -73,6 +64,11 @@ document.querySelectorAll('.check-phone-number').forEach((dataInput) => {
             mainParent.classList.remove('form-section-error');
     })
 });
+const emailListener=document.querySelector('.email-validation');
+    emailListener.addEventListener('focus',()=>{
+    const mainParent=emailListener.closest('.form-section');
+            mainParent.classList.remove('form-section-email-error');
+})
 /*end listeners*/
 
 const personForm = document.getElementsByClassName('person-form')[0];
@@ -105,18 +101,21 @@ function checkRequired(selector){
     return fullName;
 }
 function checkEmail(){
-
+    const email=document.querySelector('.email-validation');
+    if(email.value==''){
+        isItOkayToSubmit=true;
+        data[email.name]='Not givin';
+    }
+    else{
+        const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if(email.value.match(mailFormat)){
+            isItOkayToSubmit=true;
+        data[email.name]=email.value;
+        }
+        else{
+            const mainParent=email.closest('.form-section');
+            mainParent.classList.add('form-section-email-error');
+            isItOkayToSubmit=false;
+        }
+    }
 }
-
-
-/*
-document.querySelectorAll('.fullNameChecker').forEach((dataInput) => {
-            if (dataInput.value == '') {
-                const mainParent=dataInput.closest('.form-section');
-                mainParent.classList.add('form-section-error');
-                // mainParent.setAttribute('class','ok')
-                // console.log(dataInput.closest('.form-section'));
-                               
-            }
-        })
-*/
